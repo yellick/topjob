f2979167f433c1535ffd6290b945d04d3cbfdba6
Mikhail <gubm04@mail.ru>
2025-05-19 19:23:36 +0500

    fix

--- get_src.php
+++ get_src.php
@@ -1,12 +1,21 @@
 <?php
-$protocol = isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? 'https://' : 'http://';
+// Определяем протокол
+if (
+    (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on') ||
+    (isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] === 'https')
+) {
+    $protocol = 'https://';
+} else {
+    $protocol = 'http://';
+}
+
 $host = $_SERVER['HTTP_HOST'];
 $scriptPath = dirname($_SERVER['SCRIPT_NAME']);
 $projectDepth = substr_count(str_replace('\\', '/', realpath(__DIR__)), '/');
 $basePath = implode('/', array_slice(explode('/', $scriptPath), 0, -$projectDepth));
 $baseUrl = rtrim("$protocol$host$basePath", '/') . '/';
 
-// На локалхосте добавляется TopJob в путь
+// Локальный хост (если нужно)
 if ($host === 'localhost' || $host === '127.0.0.1') {
     $baseUrl .= 'TopJob/';
 }
